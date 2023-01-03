fed478123b5a76c8d0644450786583b87f5b0363
Martins Kayode <martinsk09@gmail.com>
2023-01-03 10:30:23 +0100

    INIT

---
+++ migrations/sqls/20221230152723-store-front-table-up.sql
@@ -0,0 +1,8 @@
+/* Replace with your SQL commands */
+CREATE TABLE users (
+id SERIAL PRIMARY KEY  NOT NULL,
+firstName VARCHAR(50) NOT NULL,
+lastName VARCHAR(50) NOT NULL,
+password VARCHAR(50) NOT NULL
+
+);
